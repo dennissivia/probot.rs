6a3d3d14eb834b82b549879a73141aa10fdff07e
Dennis Sivia <dennis.sivia@gmail.com>
2019-07-25 23:31:32 +0200

    Starts event endpoint implementation
    After researching a way to distinguish payloads, I learned, that we
    can use a custom  X header.
    Based on that we are now able to define a Push struct that mirrors
    the Push event so that we can extract the commit message and
    create a check-run via graphql.
    
    Next we wait for the check-run to complete to be happy.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate json;
 
+use actix_web::http::header::HeaderMap;
 use actix_web::{error, middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer};
 use bytes::BytesMut;
 use futures::{Future, Stream};
@@ -8,20 +9,87 @@ use json::JsonValue;
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-struct MyObj {
+struct CheckRun {
+    id: u32,
+    node_id: String,
+    head_sha: String,
+    status: String,
+}
+#[derive(Debug, Serialize, Deserialize)]
+struct CheckRunPayload {
     name: String,
     number: i32,
+    check_run: CheckRun,
+}
+const MAX_SIZE: usize = 262_144; // max payload size is 256k
+
+fn event_header_value(headers: &HeaderMap) -> Option<String> {
+    let maybeHeader = headers.get("X-GITHUB-EVENT");
+    match maybeHeader {
+        Some(header) => {
+            println!("{:?}", header);
+        }
+        None => {
+            println!("Did not find Event header to parse data");
+        }
+    }
+    maybeHeader.and_then(|header| header.to_str().ok().map(String::from))
 }
 
-/// This handler uses json extractor
-fn index(item: web::Json<MyObj>) -> HttpResponse {
-    println!("model: {:?}", &item);
-    HttpResponse::Ok().json(item.0) // <- send response
+fn extract_payload(payload: web::Payload) -> impl Future<Item = CheckRunPayload, Error = Error> {
+    payload
+        // `Future::from_err` acts like `?` in that it coerces the error type from
+        // the future into the final error type
+        .from_err()
+        // `fold` will asynchronously read each chunk of the request body and
+        // call supplied closure, then it resolves to result of closure
+        .fold(BytesMut::new(), move |mut body, chunk| {
+            if (body.len() + chunk.len()) > MAX_SIZE {
+                Err(error::ErrorBadRequest("overflow"))
+            } else {
+                body.extend_from_slice(&chunk);
+                Ok(body)
+            }
+        })
+        // `Future::and_then` can be used to merge an asynchronous workflow with a
+        // synchronous workflow
+        .and_then(|body| {
+            // body is loaded, now we can deserialize serde-json
+            let obj = serde_json::from_slice::<CheckRunPayload>(&body)?;
+            Ok(obj)
+            // Ok(HttpResponse::Ok().json(obj)) // <- send response
+        })
 }
 
-fn github_events(item: web::Json<MyObj>) -> HttpResponse {
-    println!("model: {:?}", &item);
-    HttpResponse::Ok().json(item.0) // <- send response
+// fn github_events(item: web::Json<CheckRunPayload>) -> HttpResponse {
+// fn github_events(req: HttpRequest, pl: web::Payload) -> impl Future<Item = HttpResponse, Error = Error> {
+fn github_events(req: HttpRequest, payload: web::Payload) -> HttpResponse {
+    let event_type = event_header_value(req.headers());
+
+    match event_type {
+        Some(header) => {
+            match header.as_str() {
+                "check_suite" => {
+                    println!("{:?}", &req);
+                    // println!("model: {:?}", payload);
+                    let data = extract_payload(payload).wait();
+                    println!("model: {:?}", data);
+                    // .map(|obj| {
+                    //       println!("{:?}", obj);
+                    //       obj
+                    //     })
+                }
+                _ => println!("Unknown event type: {:?}", header),
+            }
+        }
+        None => {}
+    }
+    //    HttpResponse::Ok().json(item.0)
+    HttpResponse::Ok().content_type("application/json").body("")
+
+    // HttpResponse::Ok()
+    //     .content_type("text/html")
+    //     .body("Setup successful. Imagine nice html")
 }
 
 // return a nice page or just redirect somewhere? Dunno...
@@ -44,7 +112,7 @@ pub fn start() -> std::io::Result<()> {
             .data(web::JsonConfig::default().limit(409600)) // <- limit size of the payload (global configuration)
             .service(web::resource("/github/setup").route(web::get().to(github_setup)))
             .service(web::resource("/github/events").route(web::post().to(github_events)))
-            .service(web::resource("/").route(web::post().to(index)))
+        //X-GitHub-Event
     })
     .bind("127.0.0.1:9999")?
     .run()
